441e724c875f0acf9f4b66734b54fc6316e6932f
padminiu <padhmini.u@gmail.com>
2023-02-25 15:19:51 +0530

    chnge 3

--- src/main/java/com/maventest/Jenkinsmaventest/Maventest2.java
+++ src/main/java/com/maventest/Jenkinsmaventest/Maventest2.java
@@ -3,7 +3,8 @@ package com.maventest.Jenkinsmaventest;
 public class Maventest2 {
 
 	public static void main(String[] args) {
-		System.out.println("new maven test 1");
+		System.out.println("new maven test1");
+		System.out.println("new maven tes12");
 
 	}
 
